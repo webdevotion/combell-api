8714e22db4c9c9abfddff84c8ad4fcc629efb8c5
Bram Plessers <webdevotion@gmail.com>
2018-06-05 22:15:04 +0200

    Converted gulpfile to ES6 ( with working ESlinting )

---
+++ gulpfile.babel.js
@@ -0,0 +1,42 @@
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var eslint = require("gulp-eslint");
+
+import gulp from 'gulp';
+
+var paths = {
+  scripts: {
+    src: ['src/**/*.js','!src/test/**/*.js'],
+    dest: 'dist/'
+  }
+};
+
+function lint() {
+    return gulp.src(['./src/**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error
+        .pipe(eslint.failAfterError());
+};
+
+function watch() {
+  gulp.watch(paths.scripts.src, build);
+};
+
+function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.scripts.dest));
+};
+
+var build = gulp.series(lint,scripts);
+
+gulp.task('build', build);
+gulp.task('lint', lint);
+gulp.task('default', gulp.series(build,watch));
+
+exports.build = build;
+exports.watch = build;
+exports.scripts = scripts;
