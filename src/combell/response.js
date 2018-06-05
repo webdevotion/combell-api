51ecef60cc10db08259d6db66d89484eb574eff2
Bram Plessers <webdevotion@gmail.com>
2018-06-05 22:22:34 +0200

    Response tries to throw the appropriate Error, if needed
    - since the implementation mainly handles errors we might need to change the name of the module down the line

---
+++ src/combell/response.js
@@ -0,0 +1,69 @@
+const contexts = {
+  general: {
+    context: 'general_error',
+  },
+  request_setup: {
+    context: 'request_setup_error',
+  },
+  no_response: {
+    context: 'no_response_received',
+  },
+  auth_authorization_header_invalid_format: {
+    context: 'authentication',
+  },
+  authorization_hmac_invalid: {
+    context: 'hmac_authorization_header_invalid',
+  },
+};
+
+// error is just axios' response object
+const errorhandler = (error) => {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    // console.log(error.request.headers)
+    // console.log(error.response.status)
+    // console.log(error.response.headers)
+    const context = errorcontext(error);
+    return new Error(context);
+  } else if (error.request) {
+    // The request was made but no response w2as received
+    // `error.request` is an instance of XMLHttpRequest in the browser
+    // and an instance of http.ClientRequest in node.js
+    return new Error(contexts.no_response.context);
+  }
+
+  return new Error(contexts.general.context);
+};
+
+let errorcontext = function (error) {
+  const status = error.response.status;
+
+  switch (status) {
+    case 401:
+      return contexts.auth_authorization_header_invalid_format.context;
+    default:
+      break;
+  }
+
+  let e = error.response.data.error_code;
+  // not able to find an error code in axios response object
+  if (!e) {
+    // we have no clue what happened, throw general error
+    return contexts.general.context;
+  }
+
+  // look up a more application centered error message
+  e = contexts[e];
+  // we haven't seen this before - throw general error
+  if (!e) {
+    return contexts.general.context;
+  }
+
+  // we have a clue what's going on and throw an error
+  // which provides some application centric context
+  // for example: 'authentication failed'
+  return e.context;
+};
+
+module.exports = { errorhandler };
