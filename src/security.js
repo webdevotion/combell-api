0489c558179e887cbd9a9ba44c4ec69487926f84
Bram Plessers <webdevotion@gmail.com>
2018-06-05 22:16:31 +0200

    Security's main goal: easily create HMAC with given parameters

--- src/security.js
+++ src/security.js
@@ -1,7 +1,5 @@
 
-let Crypto = require('crypto');
+const Crypto = require('crypto');
 
 // generates a base 64 hmac with given text and secret
-module.exports.hmacify = (text, nonce) => {
-  return Crypto.createHmac('sha256', nonce).update(text).digest('base64');
-}
+module.exports.hmacify = (text, nonce) => Crypto.createHmac('sha256', nonce).update(text).digest('base64');
