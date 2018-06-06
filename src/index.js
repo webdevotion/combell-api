e7e90df9aee570fc26674932f731d5980f04031d
Bram Plessers <webdevotion@gmail.com>
2018-06-06 11:36:51 +0200

    Simplified call and handling to accounts.index()

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const Combell = require('./combell.js');
 
 // get /v2/accounts
-const accounts = Combell.getAccounts().then((accounts) => {
-  // accounts is an array of account objects
-});
+// result is always an array of account objects
+// even when an error is thrown you will get an empty array
+Combell.getAccounts().then(result => result);
 
 module.exports = {};
