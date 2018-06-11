84ba3e214b7c9804fc43d4505cfef193d61f6f3d
Bram Plessers <webdevotion@gmail.com>
2018-06-11 15:35:37 +0200

    use es6 imports

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/combell');
+import { account } from './lib/combell'
+
+// example usage:
+// call index() to fetch all your accounts
+// index() returns a promise
+// errors from making the call are thrown and catched inside the library
+// custom error handling / error bubbling is considered for a future update
+account.index().then(result => console.log(result))
