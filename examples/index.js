1f29cf10b167af7d2d011b62f36489dbf5ce929e
Bram Plessers <webdevotion@gmail.com>
2018-06-11 15:36:13 +0200

    Don't use dotenv in production code

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+// production apps should never use dotenv to load secrets
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const Combell = require('../index.js');
 
