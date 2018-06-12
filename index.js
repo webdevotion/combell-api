68fd0f806253d1212aef72e451ecdff10d1a4133
Bram Plessers <webdevotion@gmail.com>
2018-06-12 08:56:48 +0200

    Remove index.js example files in favor of ...
    a more structured examples directory

--- index.js
+++
@@ -1,8 +0,0 @@
-import { account } from './dist/index.esm.js'
-
-// example usage:
-// call index() to fetch all your accounts
-// index() returns a promise
-// errors from making the call are thrown and catched inside the library
-// custom error handling / error bubbling is considered for a future update
-account.index().then(result => console.log(result))
