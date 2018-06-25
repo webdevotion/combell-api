ae45ba4d197b921c1142726dd8e16c3208ac5fac
Bram Plessers <webdevotion@gmail.com>
2018-06-25 15:28:14 +0200

    Working test after migration to `class` structure

--- test/specs/combell.spec.js
+++ test/specs/combell.spec.js
@@ -1,23 +1,20 @@
-import * as chai from 'chai';
-
+import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 
 const sinon = require('sinon');
+const { expect } = chai.use(chaiAsPromised);
 
-const { expect } = chai;
-chai.use(chaiAsPromised);
-
-const subject = require('../../lib/combell');
+import Combell from '../../lib/combell';
 const accounts = require('../../lib/core/accounts');
 
 describe('Combell', () => {
   describe('fetch accounts', () => {
     it('should call accounts.index exactly once', () => {
+      let subject = new Combell('0','0');
       const mock = sinon.mock(accounts);
       mock.expects('index').once();
       subject.getAccounts();
       mock.verify();
-      mock.restore();
     });
 
     it('should return empty array when error was thrown', () => {
@@ -26,19 +23,16 @@ describe('Combell', () => {
       // throw an error like the accounts module would if appropriate
       generalError.throws('default', 'general_error');
 
-      expect(subject.getAccounts()).to.eventually.exist
-        .and.to.be.a('array')
-        .and.to.have.length(0);
+      let subject = new Combell('0','0');
+
+      expect(subject.getAccounts()).to.be.rejectedWith(Error);
 
       generalError.restore();
 
       const authenticationFailed = sinon.stub(accounts, 'index');
       // throw an error like the accounts module would if appropriate
       authenticationFailed.throws('authentication', 'authentication');
-
-      expect(subject.getAccounts()).to.eventually.exist
-        .and.to.be.a('array')
-        .and.to.have.length(0);
+      expect(subject.getAccounts()).to.be.rejectedWith(Error);
 
       authenticationFailed.restore();
     });
